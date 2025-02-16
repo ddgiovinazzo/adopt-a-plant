334cc24f81fa1ec992bc4e91f684807e97312e74
ddgiovinazzo <ddgiovinazzo@gmail.com>
2025-02-16 18:53:58 -0500

    Initial commit with basic app structure

---
+++ src/data/colors.js
@@ -0,0 +1,8 @@
+export const earthyBrown = '#6b4226';
+export const softGreen = '#a3b18a';
+export const beige = '#f5f5dc';
+export const darkBrown = '#3e2723';
+export const earthyOrange = '#d2691e';
+export const darkerOrange = '#b35916';
+export const adoptedGreen = 'rgba(0, 128, 0, 0.7)';
+export const comingSoonBlack = 'rgba(0, 0, 0, 0.7)';
